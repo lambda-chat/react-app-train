f261bbff2b4ca5fb5559bc51b22fbcd6e3129f8c
lambda-chat <chat.algebrique@gmail.com>
2022-05-03 22:48:02 +0900

    make App.tsx be empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import { FC } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
-  </div>
-);
+const App: FC = () => <div className="App">React app</div>;
 
 export default App;
